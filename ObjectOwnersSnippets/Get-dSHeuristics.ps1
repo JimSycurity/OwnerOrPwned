f06e8dd8d55c195b237ecc9c9561586dae9398f8
Jim <14374121+JimSycurity@users.noreply.github.com>
2023-09-21 20:04:33 -0500

    Update Get-dSHeuristics.ps1
    Added warning and defanged reset

--- ObjectOwnersSnippets/Get-dSHeuristics.ps1
+++ ObjectOwnersSnippets/Get-dSHeuristics.ps1
@@ -2,6 +2,7 @@
 (Get-ADObject ("CN=Directory Service,CN=Windows NT,CN=Services," + (Get-ADRootDSE).configurationNamingContext) -Properties dSHeuristics).dSHeuristics
 
 # KB5008383 Enforcement Mode for LDAP Add and LDAP Modify, Set 28th and 29th characters to 1
+### Ensure you have queried the current dSHeuristics value, recorded it, and are taking that information into account before making this change!!! ###
 Set-ADObject ("CN=Directory Service,CN=Windows NT,CN=Services," + (Get-ADRootDSE).configurationNamingContext) -replace @{dSHeuristics='00000000010000000002000000011'}
 
 
@@ -10,4 +11,4 @@ Set-ADObject ("CN=Directory Service,CN=Windows NT,CN=Services," + (Get-ADRootDSE
 00000000010000000002000000011
 
 # Set Default dSHeuristics #
-Set-ADObject ("CN=Directory Service,CN=Windows NT,CN=Services," + (Get-ADRootDSE).configurationNamingContext) -clear dSHeuristics
+# Set-ADObject ("CN=Directory Service,CN=Windows NT,CN=Services," + (Get-ADRootDSE).configurationNamingContext) -clear dSHeuristics
