379e4e7e5e67be486005b6ac84d1d654969bdfb6
Jim Sykora <14374121+JimSycurity@users.noreply.github.com>
2025-06-17 15:05:08 -0500

    Update Get-dSHeuristics.ps1

--- ObjectOwnersSnippets/Get-dSHeuristics.ps1
+++ ObjectOwnersSnippets/Get-dSHeuristics.ps1
@@ -1,6 +1,10 @@
 # Query Current dSHeuristics in current AD Forest
 (Get-ADObject ("CN=Directory Service,CN=Windows NT,CN=Services," + (Get-ADRootDSE).configurationNamingContext) -Properties dSHeuristics).dSHeuristics
 
+# Query Current dSHeuristics in current AD Forest without AD PowerShell Module:
+$config = ([adsi]"LDAP://RootDSE").configurationNamingContext
+([adsi]"LDAP://CN=Directory Service,CN=Windows NT,CN=Services,$config").dSHeuristics
+
 # KB5008383 Enforcement Mode for LDAP Add and LDAP Modify, Set 28th and 29th characters to 1
 ### Ensure you have queried the current dSHeuristics value, recorded it, and are taking that information into account before making this change!!! ###
 Set-ADObject ("CN=Directory Service,CN=Windows NT,CN=Services," + (Get-ADRootDSE).configurationNamingContext) -replace @{dSHeuristics='00000000010000000002000000011'}
